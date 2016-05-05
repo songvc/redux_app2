b5e0b18e32c8fce80ae6d9652efc384ba456637c
Vincent Song <songvc@users.noreply.github.com>
2016-05-04 22:31:06 -0700

    book list redux app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: "Javascript: the Good Parts"},
+    { title: "Harry Potter"},
+    { title: "The Dark Tower"},
+    { title: "Eloquent Ruby"}
+  ]
+}
